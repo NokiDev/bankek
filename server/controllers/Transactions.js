6a5b5387d18ba9cee041819b09dce0998affec53
Ronan <ronanpsf@gmail.com>
2017-11-12 16:20:52 +0100

    cron v2 working

--- server/controllers/Transactions.js
+++ server/controllers/Transactions.js
@@ -1,74 +1,70 @@
 var mongoose = require('mongoose'),
-  chalk = require('chalk'),
-  jwt = require('jsonwebtoken'),
-  Transaction = require('../models/Transaction');
+chalk = require('chalk'),
+jwt = require('jsonwebtoken'),
+Transaction = require('../models/Transaction');
 User = require('../models/User');
 Users = require('../controllers/Users');
 var cron = require('node-cron');
 var request = require('request');
 
 let Transactions = {
-  sendTransaction: function (req, res) {
-    console.log(req.body);
-    let t = new Transaction({
-      title: req.body.title,
-      amount: req.body.amount,
-      date: Date.now(),
-      user_id: jwt.decode(req.session.usertoken)
-    });
-    t.save(
-      function () {
-        console.log("dd")
-        res.sendStatus(200);
-      },
-      function(){
-        console.log("ici?")
-      }
-    );
-
-  },
-  getTransactions: function (req, res) {
-    Transaction.find({user_id: jwt.decode(req.session.usertoken)}, (err, transactions) => {
-      if (err) {
-        throw err;
-        res.sendStatus(404);
-      } else {
-        res.status(200).json({
-          transactions: transactions
-        });
-      }
-    });
-  },
-  sendCronTransaction: function(req,res){ 
-    var fixedReq = req;    
-    var fixedEvery;
-    switch(req.body.every){
-      case "secondes":
-        fixedEvery = " * * * * * *";
-      case "minutes":
-        fixedEvery = " * * * * *";
-      case "heures":
-        fixedEvery = " * * * *";
-      case "jours":
-        fixedEvery = " * * *";
-      case "mois":
-        fixedEvery = " * *";
-      case "années":
-        fixedEvery = " *";
-      default:
-        fixedEvery = " * *";
+sendTransaction: function (req, res) {
+  createTransaction(req.body.title, req.body.amount,jwt.decode(req.session.usertoken), (err, transaction) => {
+    res.sendStatus(200);
+  })
+},
+getTransactions: function (req, res) {
+  Transaction.find({user_id: jwt.decode(req.session.usertoken)}, (err, transactions) => {
+    if (err) {
+      throw err;
+      res.sendStatus(404);
+    } else {
+      res.status(200).json({
+        transactions: transactions
+      });
     }
-    var options = {
-      url: 'http://localhost:3000/transactions/addTransaction',
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      form: req.body,
-      session: req.session
-    };
-    cron.schedule("*/1 * * * * * *",() => request(options))
+  });
+},
+sendCronTransaction: function(req,res){
+  var fixedReq = req;
+  var fixedEvery;
+  switch(req.body.every){
+    case "secondes":
+      fixedEvery = " * * * * * *";
+      break;
+    case "minutes":
+      fixedEvery = " * * * * *";
+      break;
+    case "heures":
+      fixedEvery = " * * * *";
+      break;
+    case "jours":
+      fixedEvery = " * * *";
+      break;
+    case "mois":
+      fixedEvery = " * *";
+      break;
+    case "années":
+      fixedEvery = " *";
+      break;      
+    default:
+      fixedEvery = " * *";
   }
+
+  cron.schedule("*/1 * * * * * *", function() {
+      createTransaction(req.body.title, req.body.amount, jwt.decode(req.session.usertoken, ()=> {}));
+  }, true);
+}
+};
+
+function createTransaction(title, amount, user_id, onSave) {
+let t = new Transaction({
+  title: title,
+  amount: amount,
+  date: Date.now(),
+  user_id: user_id
+});
+t.save(onSave);
 }
 
 module.exports = Transactions;
